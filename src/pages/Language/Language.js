b4ffa3f1f07c0f04f66584563c01f402ff783d9e
Michael Nguyen <n93michael@gmail.com>
2022-06-24 10:39:48 -0400

    initial styling for language page

--- src/pages/Language/Language.js
+++ src/pages/Language/Language.js
@@ -1,7 +1,25 @@
 import "./Language.scss";
 
 function Language() {
-  return <div>Language</div>;
+  return (
+    <section>
+      <section>
+        <div>
+          <Link to="/">
+            <img src="/" alt="back arrow" className="" />
+          </Link>
+        </div>
+
+        <div>
+          <div className="card"></div>
+
+          <div>
+            <Link to="/">Skip questionnaire and go back to the website</Link>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 }
 
 export default Language;
