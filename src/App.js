f3e91407f7160c19127f0b32c11dfa40870ab6c3
Michael Nguyen <n93michael@gmail.com>
2022-06-24 06:25:52 -0400

    created header component, set up routes and switches

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.scss";
+import { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
-      <h1>Hello from App </h1>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Header} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+// Router / Switches
+
+// Header / Nav bar sits on top of the routes
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello from Header </h1>
+    </>
+  );
+};
+
+export default Header;
