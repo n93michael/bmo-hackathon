f5a78671e3ad3b65c7c0f4397c85bac2bb9e355d
Michael Nguyen <n93michael@gmail.com>
2022-06-24 06:49:56 -0400

    hero page structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.scss";
-import { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
-        <Route path="/" exact component={Header} />
+        <Route path="/" exact component={Home} />
       </Switch>
     </Router>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,19 @@ import "./Header.scss";
 const Header = () => {
   return (
     <>
-      <h1>Hello from Header </h1>
+      <header className="">
+        <nav className="">
+          <div className="">
+            <img className="" alt="/" src="/" />
+          </div>
+
+          <div className="">
+            <div className="">
+              <h1>HELLO FROM HEADER</h1>
+            </div>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
