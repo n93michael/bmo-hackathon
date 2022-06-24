d57b0a66b1b511799a998adc3df8f5e867bac11e
prudziejewski <p.rudziejewski@gmail.com>
2022-06-24 10:44:46 -0400

    Finished styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,37 +1,38 @@
 import "./Home.scss";
-
 import { Link } from "react-router-dom";
+import phone from "../../assets/images/phone.png"
+import credit from "../../assets/images/credit.png"
 
 const Home = () => {
   return (
     <section className="hero">
       <div className="hero__wrapper">
         <div className="hero__left">
-          <div className="">
-            <h1 className="">
-              Earn up to $350 <span className="">*</span> when you open a
+          <div className="hero__card">
+            <h1 className="hero__text">
+              Earn up to $350 <span className="hero__text--highlighted">*</span> when you open a
               chequing account online!
             </h1>
-            <div className=""></div>
-            <p className="">
-              Plus, get access to personalized BMO insights in the BMO app. Real
+            <div className="hero__underline"></div>
+            <p className="hero__description">
+              Plus, get access to <span className="hero__description--bold">personalized BMO insights</span> in the BMO app. Real
               financial progress starts with staying on top of your money.
             </p>
           </div>
 
-          <div className="">
-            <h2 className="">New to Canada?</h2>
+          <div className="hero__cta">
+            <h2 className="hero__question">New to Canada?</h2>
 
-            <Link className="" to="/">
+            <Link className="hero__button" to="/">
               Start your journey with BMO
             </Link>
           </div>
         </div>
 
         <div className="hero__right">
-          <div className="">
-            <img className="" alt="/" src="/" />
-            <img className="" alt="/" src="/" />
+          <div className="hero__overlay">
+            <img className="hero__phone" alt="phone" src={phone} />
+            <img className="hero__credit" alt="credit card" src={credit} />
           </div>
         </div>
       </div>
