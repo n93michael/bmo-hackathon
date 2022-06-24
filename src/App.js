d3f2e0326ff4c56fe409796d323d1cc0a0342c5a
prudziejewski <p.rudziejewski@gmail.com>
2022-06-24 06:10:41 -0400

    created folder structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <>
       <h1>Hello from App </h1>
+      
+      <div className="card">
+        
+        <div className="cta">
+          <button className="button">Yes</button>
+          <button className="button">No</button>
+        </div>
+      </div>
     </>
   );
 }
