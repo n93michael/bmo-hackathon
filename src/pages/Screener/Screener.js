1e7c522492e6fa3b6a51f8c93f75d3465fa694e1
Bina Basiri <bina712010@gmail.com>
2022-06-24 15:06:58 -0400

    importing backArrow and Link}

--- src/pages/Screener/Screener.js
+++ src/pages/Screener/Screener.js
@@ -1,9 +1,8 @@
 import './Screener.scss';
+import { Link } from 'react-router-dom';
+import backArrow from '../../assets/images/Back.png';
 
 function Screener() {
-<<<<<<< HEAD
-  return <div>Screener</div>;
-=======
   return (
     <section className="language">
       <section className="language__wrapper">
@@ -16,11 +15,15 @@ function Screener() {
         <div className="language__right">
           <div className="card">
             <div className="screener">
-              <div className="screener__title"> 
-              <h1 className="screener__text">Are you familiar with Canadian banking?</h1>
+              <div className="screener__title">
+                <h1 className="screener__text">
+                  Are you familiar with Canadian banking?
+                </h1>
                 <div className="screener__cta">
                   <div className="button">Yes</div>
-                  <Link to="/learnmore" className="screener__link"><div className="button">No</div></Link>
+                  <Link to="/learnmore" className="screener__link">
+                    <div className="button">No</div>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -35,7 +38,6 @@ function Screener() {
       </section>
     </section>
   );
->>>>>>> develop
 }
 
 export default Screener;
