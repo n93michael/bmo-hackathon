7c93f8e566751458b583aee5ba7ee9b2a927d1d7
Michael Nguyen <n93michael@gmail.com>
2022-06-24 10:53:30 -0400

    BEM naming done

--- src/pages/Language/Language.js
+++ src/pages/Language/Language.js
@@ -1,20 +1,23 @@
 import "./Language.scss";
+import { Link } from "react-router-dom";
 
 function Language() {
   return (
-    <section>
-      <section>
-        <div>
-          <Link to="/">
-            <img src="/" alt="back arrow" className="" />
+    <section className="language">
+      <section className="language__wrapper">
+        <div className="language__left">
+          <Link to="/" className="return">
+            <img src="/" alt="back arrow" className="return__img" />
           </Link>
         </div>
 
-        <div>
+        <div className="language__right">
           <div className="card"></div>
 
-          <div>
-            <Link to="/">Skip questionnaire and go back to the website</Link>
+          <div className="skip">
+            <Link to="/" className="skip__link">
+              Skip questionnaire and go back to the website
+            </Link>
           </div>
         </div>
       </section>
